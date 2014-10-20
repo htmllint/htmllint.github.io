26b1655a4ea115a552bb99f8346637a63edb460e
Coalman <tehcoalman@gmail.com>
2014-10-20 11:44:17 -0400

    added react side panel

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,16 +1,53 @@
+/** @jsx React.DOM */
 var ace = require('ace'),
-    htmllint = require('htmllint');
+    htmllint = require('htmllint'),
+    React = require('react');
 
 var editor = ace.edit('editor');
 editor.setTheme('ace/theme/monokai');
 editor.getSession().setMode('ace/mode/html');
 
 var updateTimer = null;
-editor.on('change', function () {
+editor.on('change', () => {
     window.clearTimeout(updateTimer);
     updateTimer = window.setTimeout(lint, 500);
 });
 
+var IssueList = React.createClass({
+    render: function () {
+        var issueNodes = this.props.data.map((issue) => {
+            return <Issue issue={issue}></Issue>;
+        });
+
+        return (
+	    <div>
+	    {issueNodes}
+	    </div>
+	);
+    }
+});
+
+var Issue = React.createClass({
+    render: function () {
+        var issue = this.props.issue;
+
+        return (
+	    <div>
+            <span>({issue.line}, {issue.column}): </span>
+            <span>{issue.msg}</span>
+            </div>
+	);
+    }
+});
+
+var render = (issues) => {
+    React.renderComponent(
+	<IssueList data={issues}></IssueList>,
+	document.getElementById('issue-list')
+    );
+};
+
+render([]);
 function lint() {
     var html = editor.getValue(),
         issues = [];
@@ -18,11 +55,13 @@ function lint() {
     try {
         window.htmllint = htmllint;
         issues = htmllint(html);
-    } catch (e) {}
+    } catch (e) {
+        console.error(e);
+    }
 
     var annotations = [];
 
-    issues.forEach(function (issue) {
+    issues.forEach((issue) => {
         annotations.push({
             // 0 based index
             row: issue.line - 1,
@@ -32,5 +71,6 @@ function lint() {
         });
     });
 
+    render(issues);
     editor.getSession().setAnnotations(annotations);
 };
